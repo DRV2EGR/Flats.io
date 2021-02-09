0f6daa433fce5e2b8984fad84da0ae9355b7863c
DRV2NVA <1664286@gmail.com>
2021-02-09 11:44:22 +0300

    Initial commit

---
+++ src/main/java/io/flats/FlatsApplication.java
@@ -0,0 +1,13 @@
+package io.flats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatsApplication.class, args);
+    }
+
+}
