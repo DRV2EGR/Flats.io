61f165f659de6cd60b3f7d85256f29bc2f0aa063
VN <vituchina-natasha11@yandex.ru>
2021-02-14 20:33:07 +0300

    Убрана повторяющаяся dependency. Обновлён контроллер.

--- src/main/java/io/flats/controller/restfull.java
+++ src/main/java/io/flats/controller/restfull.java
@@ -2,8 +2,17 @@ package io.flats.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("hello")
+@RequestMapping("/hello")
 public class restfull {
+    @RequestMapping("/me")
+    @ResponseBody
+    public String returnMe() {
+
+
+        return "OWOUWU";
+    }
+
 }
