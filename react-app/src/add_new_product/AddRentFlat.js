cd069b49421ad71d1a26e16aec5ca1abe786c576
DRV2VNA <1664286@gmail.com>
2021-03-19 18:09:43 +0300

    Добавлены основные страницы. Требуют разработки.

---
+++ react-app/src/add_new_product/AddRentFlat.js
@@ -0,0 +1,1 @@
+//TODO: AddRentFlat.js
--- src/main/java/io/flats/controller/UserBasicController.java
+++ src/main/java/io/flats/controller/UserBasicController.java
@@ -4,14 +4,17 @@ import io.flats.dto.UserProfileImageUrlDto;
 import io.flats.entity.User;
 import io.flats.exception.UserNotFoundExeption;
 import io.flats.repository.UserRepository;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.AuthProvider;
 import java.util.ArrayList;
 
 /**
@@ -34,6 +37,8 @@ public class UserBasicController {
      */
     @RequestMapping("get_user_img_url_by_username")
     public ResponseEntity<UserProfileImageUrlDto> getUserProfileImageUrlByUsername(@RequestParam String username) { //TODO: сменить id на токен
+        System.out.println("d");
+
         //String url = "https://iconorbit.com/icons/256-watermark/1611201511385554301-Girl%20User.jpg";
         String url = userRepository.findByUsername(username).orElseThrow(
                 () -> {throw new UserNotFoundExeption();}
