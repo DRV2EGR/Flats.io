a14a30a8a101153a160094d051cc44aaa24dbb47
DRV2VNA <1664286@gmail.com>
2021-03-26 11:14:31 +0300

    Добавлен контроллер для квартир

---
+++ src/main/java/io/flats/controller/FlatController.java
@@ -0,0 +1,38 @@
+package io.flats.controller;
+
+import io.flats.dto.BasicResponce;
+import io.flats.entity.Flat;
+import io.flats.payload.FlatDtoPayload;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class FlatController {
+    /*
+        addFlat
+        delete flat
+        flat/id - see flat = getFlat
+
+     */
+
+    public ResponseEntity<BasicResponce> addFlat(@RequestBody FlatDtoPayload newFlat) {
+        //TODO: addFlat
+
+        return null;
+    }
+
+    public ResponseEntity<BasicResponce> deleteFlat(@RequestBody Long id) {
+        //TODO: deleteFlat
+
+        return null;
+    }
+
+    public ResponseEntity<BasicResponce> getFlat(@RequestBody Long id) {
+        //TODO: getFlat
+
+        return null;
+    }
+}
