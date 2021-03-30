482220336231f8e67cae9b3ce427ee0eabceb411
DRV2VNA <1664286@gmail.com>
2021-03-30 19:55:40 +0300

    Добавлен FlatsImagesRepository

---
+++ src/main/java/io/flats/repository/FlatsImagesRepository.java
@@ -0,0 +1,7 @@
+package io.flats.repository;
+
+import io.flats.entity.FlatsImages;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlatsImagesRepository extends JpaRepository<FlatsImages, Long> {
+}
