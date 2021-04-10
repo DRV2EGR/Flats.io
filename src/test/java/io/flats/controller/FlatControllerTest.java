0d94e93570e0822025db70e41853e3ec03a5f90c
DRV2VNA <1664286@gmail.com>
2021-04-10 22:10:44 +0300

    Добавлены дополнительные тесты.

--- src/test/java/io/flats/controller/FlatControllerTest.java
+++ src/test/java/io/flats/controller/FlatControllerTest.java
@@ -107,6 +107,68 @@ public class FlatControllerTest {
     }
 
     @Test
+    public void testAddFlat2() throws Exception {
+        FlatOrderType flatOrderType = new FlatOrderType();
+        flatOrderType.setId(123L);
+        flatOrderType.setName("Name");
+
+        Role role = new Role();
+        role.setId(123L);
+        role.setName("Name");
+
+        User user = new User();
+        user.setLastName("Doe");
+        user.setEmail("jane.doe@example.org");
+        user.setPassword("iloveyou");
+        user.setActivationCode("Activation Code");
+        user.setUsername("janedoe");
+        user.setSecondName("Second Name");
+        user.setId(123L);
+        user.setPhoneNumber("4105551212");
+        user.setTimeOfAccountCreation(LocalDateTime.of(1, 1, 1, 1, 1));
+        user.setUserProfileImageUrl("https://example.org/example");
+        user.setFirstName("Jane");
+        user.setRole(role);
+
+        Flat flat = new Flat();
+        flat.setFlatsImages(new ArrayList<FlatsImages>());
+        flat.setPrice(10.0f);
+        flat.setCountry("Country");
+        flat.setFloor(1);
+        flat.setId(123L);
+        flat.setOrderType(flatOrderType);
+        flat.setTown("Oxford");
+        flat.setOwner(user);
+        flat.setDescription("The characteristics of someone or something");
+        flat.setHouseNom("House Nom");
+        flat.setStreet("Street");
+        when(this.flatService.addRentFlat((FlatDtoPayload) any())).thenReturn(flat);
+
+        FlatDtoPayload flatDtoPayload = new FlatDtoPayload();
+        flatDtoPayload.setFlatsImages(new ArrayList<String>());
+        flatDtoPayload.setPrice(10.0f);
+        flatDtoPayload.setUsername("janedoe");
+        flatDtoPayload.setCountry("Country");
+        flatDtoPayload.setFloor(1);
+        flatDtoPayload.setTown("Oxford");
+        flatDtoPayload.setForSale(false);
+        flatDtoPayload.setDescription("The characteristics of someone or something");
+        flatDtoPayload.setForRent(true);
+        flatDtoPayload.setHouseNom("House Nom");
+        flatDtoPayload.setStreet("Street");
+        String content = (new ObjectMapper()).writeValueAsString(flatDtoPayload);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/api/flats/add_flat")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+        MockMvcBuilders.standaloneSetup(this.flatController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("{\"response\":\"OK\"}")));
+    }
+
+    @Test
     public void testGetFlat() throws Exception {
         FlatOrderType flatOrderType = new FlatOrderType();
         flatOrderType.setId(123L);
@@ -159,6 +221,151 @@ public class FlatControllerTest {
 
     @Test
     public void testGetFlat2() throws Exception {
+        FlatOrderType flatOrderType = new FlatOrderType();
+        flatOrderType.setId(123L);
+        flatOrderType.setName("?");
+
+        Role role = new Role();
+        role.setId(123L);
+        role.setName("?");
+
+        User user = new User();
+        user.setLastName("Doe");
+        user.setEmail("jane.doe@example.org");
+        user.setPassword("iloveyou");
+        user.setActivationCode("?");
+        user.setUsername("janedoe");
+        user.setSecondName("?");
+        user.setId(123L);
+        user.setPhoneNumber("4105551212");
+        user.setTimeOfAccountCreation(LocalDateTime.of(1, 1, 1, 1, 1));
+        user.setUserProfileImageUrl("https://example.org/example");
+        user.setFirstName("Jane");
+        user.setRole(role);
+
+        Flat flat = new Flat();
+        flat.setFlatsImages(new ArrayList<FlatsImages>());
+        flat.setPrice(10.0f);
+        flat.setCountry("?");
+        flat.setFloor(0);
+        flat.setId(123L);
+        flat.setOrderType(flatOrderType);
+        flat.setTown("Oxford");
+        flat.setOwner(user);
+        flat.setDescription("The characteristics of someone or something");
+        flat.setHouseNom("?");
+        flat.setStreet("?");
+
+        FlatsImages flatsImages = new FlatsImages();
+        flatsImages.setImgUrl("https://example.org/example");
+        flatsImages.setId(123L);
+        flatsImages.setFlat(flat);
+
+        ArrayList<FlatsImages> flatsImagesList = new ArrayList<FlatsImages>();
+        flatsImagesList.add(flatsImages);
+
+        FlatOrderType flatOrderType1 = new FlatOrderType();
+        flatOrderType1.setId(123L);
+        flatOrderType1.setName("Name");
+
+        Role role1 = new Role();
+        role1.setId(123L);
+        role1.setName("Name");
+
+        User user1 = new User();
+        user1.setLastName("Doe");
+        user1.setEmail("jane.doe@example.org");
+        user1.setPassword("iloveyou");
+        user1.setActivationCode("Activation Code");
+        user1.setUsername("janedoe");
+        user1.setSecondName("Second Name");
+        user1.setId(123L);
+        user1.setPhoneNumber("4105551212");
+        user1.setTimeOfAccountCreation(LocalDateTime.of(1, 1, 1, 1, 1));
+        user1.setUserProfileImageUrl("https://example.org/example");
+        user1.setFirstName("Jane");
+        user1.setRole(role1);
+
+        Flat flat1 = new Flat();
+        flat1.setFlatsImages(flatsImagesList);
+        flat1.setPrice(10.0f);
+        flat1.setCountry("Country");
+        flat1.setFloor(1);
+        flat1.setId(123L);
+        flat1.setOrderType(flatOrderType1);
+        flat1.setTown("Oxford");
+        flat1.setOwner(user1);
+        flat1.setDescription("The characteristics of someone or something");
+        flat1.setHouseNom("House Nom");
+        flat1.setStreet("Street");
+        when(this.flatService.findFlatById(anyLong())).thenReturn(flat1);
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/api/flats/get_flat");
+        MockHttpServletRequestBuilder requestBuilder = getResult.param("id", String.valueOf(1L));
+        MockMvcBuilders.standaloneSetup(this.flatController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string(Matchers.containsString(
+                                "{\"country\":\"Country\",\"town\":\"Oxford\",\"street\":\"Street\",\"houseNom\":\"House Nom\",\"floor\":1,\"price\":10.0"
+                                        + ",\"description\":\"The characteristics of someone or something\",\"images\":[\"https://example.org/example\""
+                                        + "],\"id\":123,\"ownerUsername\":\"janedoe\",\"ownerID\":123}")));
+    }
+
+    @Test
+    public void testGetFlat3() throws Exception {
+        FlatOrderType flatOrderType = new FlatOrderType();
+        flatOrderType.setId(123L);
+        flatOrderType.setName("Name");
+
+        Role role = new Role();
+        role.setId(123L);
+        role.setName("Name");
+
+        User user = new User();
+        user.setLastName("Doe");
+        user.setEmail("jane.doe@example.org");
+        user.setPassword("iloveyou");
+        user.setActivationCode("Activation Code");
+        user.setUsername("janedoe");
+        user.setSecondName("Second Name");
+        user.setId(123L);
+        user.setPhoneNumber("4105551212");
+        user.setTimeOfAccountCreation(LocalDateTime.of(1, 1, 1, 1, 1));
+        user.setUserProfileImageUrl("https://example.org/example");
+        user.setFirstName("Jane");
+        user.setRole(role);
+
+        Flat flat = new Flat();
+        flat.setFlatsImages(new ArrayList<FlatsImages>());
+        flat.setPrice(10.0f);
+        flat.setCountry("Country");
+        flat.setFloor(1);
+        flat.setId(123L);
+        flat.setOrderType(flatOrderType);
+        flat.setTown("Oxford");
+        flat.setOwner(user);
+        flat.setDescription("The characteristics of someone or something");
+        flat.setHouseNom("House Nom");
+        flat.setStreet("Street");
+        when(this.flatService.findFlatById(anyLong())).thenReturn(flat);
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/api/flats/get_flat");
+        MockHttpServletRequestBuilder requestBuilder = getResult.param("id", String.valueOf(1L));
+        MockMvcBuilders.standaloneSetup(this.flatController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.content()
+                        .string(Matchers.containsString(
+                                "{\"country\":\"Country\",\"town\":\"Oxford\",\"street\":\"Street\",\"houseNom\":\"House Nom\",\"floor\":1,\"price\":10.0"
+                                        + ",\"description\":\"The characteristics of someone or something\",\"images\":[],\"id\":123,\"ownerUsername\":"
+                                        + "\"janedoe\",\"ownerID\":123}")));
+    }
+
+    @Test
+    public void testGetFlat4() throws Exception {
         FlatOrderType flatOrderType = new FlatOrderType();
         flatOrderType.setId(123L);
         flatOrderType.setName("?");
--- src/test/java/io/flats/dto/FlatDtoTest.java
+++ src/test/java/io/flats/dto/FlatDtoTest.java
@@ -12,13 +12,45 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FlatDtoTest {
 
     @org.junit.jupiter.api.Test
+    public void testCanEqual() {
+        assertFalse((new FlatDto()).canEqual("Other"));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testCanEqual2() {
+        FlatDto flatDto = new FlatDto();
+        assertTrue(flatDto.canEqual(new FlatDto()));
+    }
+
+    @org.junit.jupiter.api.Test
     public void testConstructor() {
+        FlatDto actualFlatDto = new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L);
+        assertEquals("Country", actualFlatDto.getCountry());
+        assertEquals("FlatDto(country=Country, town=Oxford, street=Street, houseNom=House Nom, floor=1, price=10.0,"
+                + " description=The characteristics of someone or something, images=[], id=123, ownerUsername=janedoe,"
+                + " ownerID=1)", actualFlatDto.toString());
+        assertEquals("Oxford", actualFlatDto.getTown());
+        assertEquals("Street", actualFlatDto.getStreet());
+        assertEquals(10.0f, actualFlatDto.getPrice());
+        assertEquals("janedoe", actualFlatDto.getOwnerUsername());
+        assertEquals(1L, actualFlatDto.getOwnerID());
+        assertEquals(123L, actualFlatDto.getId());
+        assertEquals("House Nom", actualFlatDto.getHouseNom());
+        assertEquals(1, actualFlatDto.getFloor());
+        assertEquals("The characteristics of someone or something", actualFlatDto.getDescription());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testConstructor2() {
         FlatDto actualFlatDto = new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
                 "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L);
         assertEquals("Country", actualFlatDto.getCountry());
@@ -56,6 +88,202 @@ public class FlatDtoTest {
         assertThat(fd.getOwnerID()).isEqualTo(12L);
     }
 
+    @org.junit.jupiter.api.Test
+    public void testEquals() {
+        assertFalse((new FlatDto()).equals("42"));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals10() {
+        FlatDto flatDto = new FlatDto();
+
+        FlatDto flatDto1 = new FlatDto();
+        flatDto1.setTown("Oxford");
+        assertFalse(flatDto.equals(flatDto1));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals11() {
+        FlatDto flatDto = new FlatDto();
+
+        FlatDto flatDto1 = new FlatDto();
+        flatDto1.setOwnerUsername("janedoe");
+        assertFalse(flatDto.equals(flatDto1));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals12() {
+        FlatDto flatDto = new FlatDto();
+
+        FlatDto flatDto1 = new FlatDto();
+        flatDto1.setDescription("The characteristics of someone or something");
+        assertFalse(flatDto.equals(flatDto1));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals13() {
+        FlatDto flatDto = new FlatDto(null, "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L);
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals14() {
+        FlatDto flatDto = new FlatDto("Oxford", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L);
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals15() {
+        FlatDto flatDto = new FlatDto("Country", "Oxford", "Country", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L);
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals16() {
+        FlatDto flatDto = new FlatDto("Country", "Oxford", null, "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L);
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals17() {
+        FlatDto flatDto = new FlatDto("Country", "Oxford", "Street", "Country", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L);
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals18() {
+        FlatDto flatDto = new FlatDto("Country", "Oxford", "Street", null, 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L);
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals19() {
+        ArrayList<String> stringList = new ArrayList<String>();
+        stringList.add("Country");
+        FlatDto flatDto = new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", stringList, 123L, "janedoe", 1L);
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals2() {
+        FlatDto flatDto = new FlatDto();
+        assertTrue(flatDto.equals(new FlatDto()));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals20() {
+        FlatDto flatDto = new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 0L);
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals3() {
+        FlatDto flatDto = new FlatDto();
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals4() {
+        FlatDto flatDto = new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L);
+        assertTrue(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals5() {
+        FlatDto flatDto = new FlatDto();
+        assertFalse(flatDto.equals(new FlatDto("Country", "Oxford", "Street", "House Nom", 0, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals6() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setId(123L);
+        assertFalse(flatDto.equals(new FlatDto()));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals7() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setTown("Oxford");
+        assertFalse(flatDto.equals(new FlatDto()));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals8() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setOwnerUsername("janedoe");
+        assertFalse(flatDto.equals(new FlatDto()));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEquals9() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setDescription("The characteristics of someone or something");
+        assertFalse(flatDto.equals(new FlatDto()));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testHashCode() {
+        assertEquals(529239746, (new FlatDto()).hashCode());
+        assertEquals(-1805968433, (new FlatDto("Country", "Oxford", "Street", "House Nom", 1, 10.0f,
+                "The characteristics of someone or something", new ArrayList<String>(), 123L, "janedoe", 1L)).hashCode());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetCountry() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setCountry("Country");
+        assertEquals("Country", flatDto.getCountry());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetDescription() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setDescription("The characteristics of someone or something");
+        assertEquals("The characteristics of someone or something", flatDto.getDescription());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetFloor() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setFloor(1);
+        assertEquals(1, flatDto.getFloor());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetHouseNom() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setHouseNom("House Nom");
+        assertEquals("House Nom", flatDto.getHouseNom());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetId() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setId(123L);
+        assertEquals(123L, flatDto.getId());
+    }
+
     @Test
     public void testSetImageListToNew() {
         FlatDto fd = new FlatDto();
@@ -70,5 +298,62 @@ public class FlatDtoTest {
         flatDto.setImageListToNew();
         assertEquals("FlatDto(country=null, town=null, street=null, houseNom=null, floor=0, price=0.0, description=null,"
                 + " images=[], id=0, ownerUsername=null, ownerID=0)", flatDto.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetImageListToNew3() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setImageListToNew();
+        assertEquals("FlatDto(country=null, town=null, street=null, houseNom=null, floor=0, price=0.0, description=null,"
+                + " images=[], id=0, ownerUsername=null, ownerID=0)", flatDto.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetImages() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setImages(new ArrayList<String>());
+        assertEquals("FlatDto(country=null, town=null, street=null, houseNom=null, floor=0, price=0.0, description=null,"
+                + " images=[], id=0, ownerUsername=null, ownerID=0)", flatDto.toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetOwnerID() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setOwnerID(1L);
+        assertEquals(1L, flatDto.getOwnerID());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetOwnerUsername() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setOwnerUsername("janedoe");
+        assertEquals("janedoe", flatDto.getOwnerUsername());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetPrice() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setPrice(10.0f);
+        assertEquals(10.0f, flatDto.getPrice());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetStreet() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setStreet("Street");
+        assertEquals("Street", flatDto.getStreet());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSetTown() {
+        FlatDto flatDto = new FlatDto();
+        flatDto.setTown("Oxford");
+        assertEquals("Oxford", flatDto.getTown());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testToString() {
+        assertEquals("FlatDto(country=null, town=null, street=null, houseNom=null, floor=0, price=0.0, description=null,"
+                + " images=null, id=0, ownerUsername=null, ownerID=0)", (new FlatDto()).toString());
     }
 }
