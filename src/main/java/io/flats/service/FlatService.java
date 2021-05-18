b780507a6f69db93010d11578fa6751169e7ba73
DRV2VNA <1664286@gmail.com>
2021-05-18 19:43:14 +0300

    FIX: auto inserts of flats images

--- src/main/java/io/flats/service/FlatService.java
+++ src/main/java/io/flats/service/FlatService.java
@@ -144,7 +144,7 @@ public class FlatService {
 
         if (newFlatDao.getFlatsImages().size() == 0) {
             FlatsImages fi = new FlatsImages();
-            fi.setImgUrl("");
+            fi.setImgUrl("https://upload.wikimedia.org/wikipedia/commons/9/9a/%D0%9D%D0%B5%D1%82_%D1%84%D0%BE%D1%82%D0%BE.png");
             fi.setFlat(findFlatById(newFlat.getId()));
             flatsImagesRepository.save(fi);
         }
